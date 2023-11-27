9d5edfef633890bbb502a1756086e8e64e1ba600
krupaliNimap <krupali@himapinfotech.com>
2023-11-27 10:41:29 +0530

    review task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const Parent14 = () => {
+  const arr = [
+    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+  ];
+
+  const refArray = Array.from({ length: arr.length }, () =>
+    React.createRef(null)
   );
-}
 
-export default App;
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(null);
+
+  const handlePlay = (index) => {
+    setCurrentVideoIndex(index);
+  };
+
+  useEffect(() => {
+    refArray.forEach((item, index) => {
+      index !== currentVideoIndex && item?.current?.pause();
+    });
+  }, [currentVideoIndex]);
+
+  return (
+    <>
+      {arr.map((item, index) => {
+        return (
+          <video
+            key={index}
+            ref={refArray[index]}
+            id={index}
+            width="500"
+            height="250"
+            controls
+            controlsList="nodownload"
+            onPlay={() => handlePlay(index)}
+          >
+            <source src={item} type="video/mp4" />
+          </video>
+        );
+      })}
+    </>
+  );
+};
+
+export default Parent14;
